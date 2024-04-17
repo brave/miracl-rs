992105063f505d893f54a1da90c5404288d8f1de
Darnell Andries <dandries@brave.com>
2024-04-16 22:10:12 -0700

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+git submodule update --init --remote
+rm -rf ./src ./target
+
+cp ./miracl-core/rust/*.rs .
+cp ./miracl-core/rust/config64.py .
+
+mv ./Cargo.toml ./Cargo.toml.bak
+
+# select bn254 curve in config
+echo -e "28\n0\n" | python3 ./config64.py
+
+rm ./*.rs ./*.py
+mv ./mcore/src/ .
+rm -rf mcore
+
+mv ./Cargo.toml.bak ./Cargo.toml
